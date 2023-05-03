fe6599f2a19d0ec5b84aad609d5c39a53bed58db
Tim Haak <tim@haak.co>
2023-05-03 17:23:37 +0200

    fixes

--- database/migrations/2015_12_11_122625_haakco_laravel_ip_create_private_ips_table.php
+++ database/migrations/2015_12_11_122625_haakco_laravel_ip_create_private_ips_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class HaakcoLaravelIpCreateIpsTable extends Migration
+class HaakcoLaravelIpCreatePrivateIpsTable extends Migration
 {
     /**
      * Run the migrations.
