538b8f65d3c73c65d704d56075985964a8862bc8
Tim Haak <tim@haak.co>
2024-01-12 22:02:39 +0200

    Update migration

---
+++ database/migrations/2015_12_12_122625_haakco_laravel_ip_add_enabled_and_slug_status.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(config('ip.tables.ip_types'), function (Blueprint $table) {
+            $table->boolean('enabled')
+                ->after('updated_at')
+                ->default(true);
+            $table->string('slug')
+                ->after('enabled')
+                ->default('');
+        });
+
+        DB::update('UPDATE ip_types SET slug = name;');
+
+        Schema::table(config('ip.tables.ip_types'), function (Blueprint $table) {
+            $table->unique(['slug']);
+        });
+
+        DB:: insert(
+            "INSERT INTO ip_types (id, family, enabled, slug, name) VALUES (0, 0, false, 'unknown', 'Unknown');"
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::delete("DELETE FROM ip_types WHERE id = 0 and slug='unknown' and name = 'Unknown';");
+        Schema::table(config('ip.tables.ip_types'), function (Blueprint $table) {
+            $table->dropColumn('enabled');
+            $table->dropColumn('slug');
+        });
+    }
+}
